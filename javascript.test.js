99d8c3cef2f5b5e1958cca2af68aef0f458ac1bd
dimzles <dimzlestv@gmail.com>
2023-06-14 18:43:55 +0100

    Add test for reverseString function

--- javascript.test.js
+++ javascript.test.js
@@ -4,3 +4,9 @@ test('Capitalise the first letter of a string', () => {
     expect(capitalise('potato')).toBe('Potato');
     expect(capitalise('straWberry')).toBe('Strawberry');
 })
+
+test('Reverse a string', () => {
+    expect('string').toBe('gnirts');
+    expect('HeLlO').toBe('OlLeH');
+    expect('bob').toBe('bob');
+})

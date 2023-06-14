9238a1fa15a6409e8a16c08c2f7e83c786dba1ca
dimzles <dimzlestv@gmail.com>
2023-06-14 19:01:33 +0100

    Add test for calculator object

--- javascript.test.js
+++ javascript.test.js
@@ -10,3 +10,10 @@ test('Reverse a string', () => {
     expect(reverseStr('HeLlO')).toBe('OlLeH');
     expect(reverseStr('bob')).toBe('bob');
 })
+
+test('Calculator doing add/subtract/divide/multiply', () => {
+    expect(calculator.add(1, 2)).toBe(3);
+    expect(calculator.subtract(7, 5)).toBe(2);
+    expect(calculator.divide(20, 5)).toBe(4);
+    expect(calculator.multiply(5, 1)).toBe(5)
+})
